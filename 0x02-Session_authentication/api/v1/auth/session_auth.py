655d1e5c6e785c1e2a27d3c54623870f32495053
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-09-08 18:50:32 +0100

    task 1 done

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,8 @@
 SessionAuth Module for handling Session authentication
 """
 from api.v1.auth.auth import Auth
+
+
 class SessionAuth(Auth):
     """
     Session Authentication class that inherit from Auth
