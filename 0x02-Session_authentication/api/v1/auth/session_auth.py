0931ebe98e8e58c4493d03ed61437a114b72c709
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-09-08 18:59:34 +0100

    task 2 done

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,20 +2,33 @@
 """
 SessionAuth Module for handling Session authentication
 """
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
-    """
-    Session Authentication class that implement session ID management.
-    """
+    """Session Authentication class that implements session ID management."""
+
+    # Class attribute to store session IDs
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """ Create a Session ID for a user_id
+        """Creates a session ID for the user_id.
+
+        Args:
+            user_id (str): The ID of the user to create a session for.
+
+        Returns:
+            str: The session ID, or None if user_id is invalid.
         """
         if user_id is None or not isinstance(user_id, str):
             return None
+
+        # Generate a session ID
         session_id = str(uuid.uuid4())
-        SessionAuth.user_id_by_session_id[session_id] = user_id
+
+        # Store the session ID with the corresponding user ID
+        self.user_id_by_session_id[session_id] = user_id
+
+        # Return the session ID
         return session_id
