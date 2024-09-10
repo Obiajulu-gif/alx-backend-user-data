632d14206cf1b23167c467d22083100ee00a56db
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-09-10 15:41:06 +0100

    task 6 done

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-import bcrypt
 """
 hash_password function that expects one string
 argument name password and returns a salted,
 hashed password, which is a byte string.
 """
+import bcrypt
 
 
 def hash_password(password: str) -> bytes:
