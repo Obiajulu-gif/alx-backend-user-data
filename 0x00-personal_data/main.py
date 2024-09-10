297c9cdacdf84d82581395040b43547c40e52f86
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-09-10 15:09:00 +0100

    task 3 done

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -5,15 +5,10 @@ Main file
 
 get_db = __import__('filtered_logger').get_db
 
-# Establish the connection to the database
 db = get_db()
 cursor = db.cursor()
-
-# Execute a simple query
 cursor.execute("SELECT COUNT(*) FROM users;")
 for row in cursor:
     print(row[0])
-
-# Close cursor and connection
 cursor.close()
 db.close()
