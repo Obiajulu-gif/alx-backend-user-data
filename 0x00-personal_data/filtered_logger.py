44314ae45d08e79c8cbadd36a7ef82d3e2daeac2
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-09-10 11:56:48 +0100

    task 0 done

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -48,6 +48,9 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """
+         filter values in incoming log records using filter_datum
+        """
         record.msg = filter_datum(
             self.fields,
             self.REDACTION,
