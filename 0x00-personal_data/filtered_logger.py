24cb4981535bd1fa72a9032503b84e806b02ea04
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-09-10 11:30:35 +0100

    task 0 done

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+ a function called filter_datum that returns the log message obfuscated
+"""
+import re
+from typing import List
+
+
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
+    pattern = '|'.join([f"{field}=.*?(?={separator}|$)" for field in fields])
+    return re.sub(
+        pattern,
+        lambda m: m.group(0).split('=')[0] +
+        '=' +
+        redaction,
+        message)
