f194ffc8b18b28e6b2d6c86f4bc46c9f8b4f136a
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-09-10 15:22:17 +0100

    task 3 done

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -93,7 +93,7 @@ def get_db() -> MySQLConnection:
     """
 
     username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = os.getenv("PERSONAL_DATA_DB_NAME")
 
