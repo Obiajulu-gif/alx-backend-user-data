2471c8dd70eabfbb1c3b326e2c05fb005589392d
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-09-11 15:39:38 +0100

    task 1 done

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,7 +6,12 @@ DB module to interact with the user database.
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
+
+VALID_FIELDS = ['id', 'email', 'hashed_password', 'session_id',
+                'reset_token']
 
 
 class DB:
@@ -46,3 +51,24 @@ class DB:
         self._session.commit()
         # Return the newly created user object
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user by arbitrary keyword arguments.
+
+        Args:
+            kwargs: Arbitrary keyword arguments (e.g., email='test@test.com')
+
+        Returns:
+            User: The first matching user.
+
+        Raises:
+            NoResultFound: If no matching user is found.
+            InvalidRequestError: If an invalid query argument is provided.
+        """
+        if not kwargs or any(x not in VALID_FIELDS for x in kwargs):
+            raise InvalidRequestError
+        session = self._session
+        try:
+            return session.query(User).filter_by(**kwargs).one()
+        except Exception:
+            raise NoResultFound
