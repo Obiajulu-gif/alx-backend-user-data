c1a0032f36f2c97170c80851300c8259e111b151
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-09-11 15:00:37 +0100

    task 1 done

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,52 +1,48 @@
-#!/usr/binenv python3
-"""DB module
+#!/usr/bin/env python3
 """
+DB module to interact with the user database.
+"""
+
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import Base, User
 
 
 class DB:
-    """DB class
-    """
+    """DB class for handling database operations."""
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        """Initialize a new DB instance with SQLite database."""
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        # Drop and create all tables
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """Memoized session object."""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """
-        add a user to the database and return the user object
+        """Adds a user to the database and returns the User object.
 
         Args:
-            email (str): the user email
-            hashed_password (str): the user hashed password
+            email (str): The user's email.
+            hashed_password (str): The user's hashed password.
 
         Returns:
             User: The created user object.
         """
-        if not email or not hashed_password:
-            return
-        
+        # Create a new User instance
         new_user = User(email=email, hashed_password=hashed_password)
-
+        # Add the user to the session
         self._session.add(new_user)
-
+        # Commit the changes to the database
         self._session.commit()
-
+        # Return the newly created user object
         return new_user
